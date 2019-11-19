546ca7307c9db87b804b379f589b1c38210cd41f
Bob Aiello <bob.aiello@ieee.org>
2019-11-19 00:02:22 +0000

    update scripts

--- weather-app/src/config.js
+++ weather-app/src/config.js
@@ -1,5 +1,6 @@
 apiKey = '22e6b672f28be4c5a1a1db53f0a4c132';
-url = `http://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=imperial`
+#url = `http://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=imperial`
+url = `api.openweathermap.org/data/2.5/forecast?id=524901&APPID=1111111111`
 
 module.exports = {
   'url': url
