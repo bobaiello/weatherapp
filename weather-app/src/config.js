8bf824bd5257e6a6f5ea0fbb2a9f95e9d464489d
Bob Aiello <bob.aiello@ieee.org>
2019-11-20 20:18:58 +0000

    update config.js

--- weather-app/src/config.js
+++ weather-app/src/config.js
@@ -1,5 +1,5 @@
 apiKey = '22e6b672f28be4c5a1a1db53f0a4c132';
-url = 'http://api.openweathermap.org/data/2.5/weather?zip=10022,us&appid=${apiKey}'
+url = `http://api.openweathermap.org/data/2.5/weather?zip=10022,us&APPID=${apiKey}`
 
 
 module.exports = {
